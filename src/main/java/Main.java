f43771d963d0756b86d10061ca51cc34b652f33f
Roman Bulinski <aboandrus@gmail.com>
2019-02-25 10:27:54 +0100

    Create main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public boolean checkPESEL(int pesel){
+
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        System.out.println(main.checkPESEL());
+
+
+    }
+
+
+}
