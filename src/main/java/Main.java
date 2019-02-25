21aafc5a23c164f3fc4d31d90bf730bf7450d6c7
Roman Bulinski <aboandrus@gmail.com>
2019-02-25 11:44:44 +0100

    Refactor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,17 @@
 public class Main {
 
-
     public boolean checkPESEL(String pesel){
-
         if(pesel.length()!=11){
             return false;
         }
-
         String[] chars = pesel.split("");
         int[] ints = new int[chars.length];
         for( int i=0; i< chars.length; i++){
              ints[i] = Integer.valueOf(chars[i]);
             System.out.println(ints[i]);
         }
-
         int[] actionInt = {1,3,7,9,1,3,7,9,1,3};
         int sumOfints = 0;
-
         for(int i=0; i< actionInt.length; i++ ){
             ints[i] = ints[i] * actionInt[i];
             sumOfints = sumOfints + ints[i];
@@ -28,12 +23,9 @@ public class Main {
         return false;
     }
 
-
     public static void main(String[] args) {
         Main main = new Main();
 //        System.out.println(main.checkPESEL(" "));
 
     }
-
-
 }
